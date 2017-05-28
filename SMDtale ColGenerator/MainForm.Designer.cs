cb8c657b9cd17334db97ac81aba7e0f05b31f224
VladislavSavvateev <savvateevvlad@mail.ru>
2017-05-28 14:10:47 +0900

    Добавьте файлы проекта.

---
+++ SMDtale ColGenerator/MainForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace SMDtale_ColGenerator {
+	partial class MainForm {
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent() {
+			this.pbImage = new System.Windows.Forms.PictureBox();
+			this.grpColor = new System.Windows.Forms.GroupBox();
+			this.btnColorPicker = new System.Windows.Forms.Button();
+			this.pbColor = new System.Windows.Forms.PictureBox();
+			this.grpFile = new System.Windows.Forms.GroupBox();
+			this.btnSave = new System.Windows.Forms.Button();
+			this.btnOpen = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+			this.grpColor.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pbColor)).BeginInit();
+			this.grpFile.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// pbImage
+			// 
+			this.pbImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pbImage.Location = new System.Drawing.Point(12, 12);
+			this.pbImage.Name = "pbImage";
+			this.pbImage.Size = new System.Drawing.Size(512, 256);
+			this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+			this.pbImage.TabIndex = 0;
+			this.pbImage.TabStop = false;
+			this.pbImage.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pbImage_MouseClick);
+			this.pbImage.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbImage_MouseMove);
+			// 
+			// grpColor
+			// 
+			this.grpColor.Controls.Add(this.btnColorPicker);
+			this.grpColor.Controls.Add(this.pbColor);
+			this.grpColor.Enabled = false;
+			this.grpColor.Location = new System.Drawing.Point(12, 274);
+			this.grpColor.Name = "grpColor";
+			this.grpColor.Size = new System.Drawing.Size(128, 58);
+			this.grpColor.TabIndex = 1;
+			this.grpColor.TabStop = false;
+			this.grpColor.Text = "Выбор цвета";
+			// 
+			// btnColorPicker
+			// 
+			this.btnColorPicker.Location = new System.Drawing.Point(44, 24);
+			this.btnColorPicker.Name = "btnColorPicker";
+			this.btnColorPicker.Size = new System.Drawing.Size(75, 23);
+			this.btnColorPicker.TabIndex = 1;
+			this.btnColorPicker.Text = "Пипетка";
+			this.btnColorPicker.UseVisualStyleBackColor = true;
+			this.btnColorPicker.Click += new System.EventHandler(this.btnColorPicker_Click);
+			// 
+			// pbColor
+			// 
+			this.pbColor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pbColor.Location = new System.Drawing.Point(6, 19);
+			this.pbColor.Name = "pbColor";
+			this.pbColor.Size = new System.Drawing.Size(32, 32);
+			this.pbColor.TabIndex = 0;
+			this.pbColor.TabStop = false;
+			// 
+			// grpFile
+			// 
+			this.grpFile.Controls.Add(this.btnSave);
+			this.grpFile.Controls.Add(this.btnOpen);
+			this.grpFile.Location = new System.Drawing.Point(146, 274);
+			this.grpFile.Name = "grpFile";
+			this.grpFile.Size = new System.Drawing.Size(317, 47);
+			this.grpFile.TabIndex = 2;
+			this.grpFile.TabStop = false;
+			this.grpFile.Text = "Открытие/сохранение";
+			// 
+			// btnSave
+			// 
+			this.btnSave.Enabled = false;
+			this.btnSave.Location = new System.Drawing.Point(152, 19);
+			this.btnSave.Name = "btnSave";
+			this.btnSave.Size = new System.Drawing.Size(159, 23);
+			this.btnSave.TabIndex = 1;
+			this.btnSave.Text = "Сохранить файл коллизий...";
+			this.btnSave.UseVisualStyleBackColor = true;
+			this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+			// 
+			// btnOpen
+			// 
+			this.btnOpen.Location = new System.Drawing.Point(6, 19);
+			this.btnOpen.Name = "btnOpen";
+			this.btnOpen.Size = new System.Drawing.Size(140, 23);
+			this.btnOpen.TabIndex = 0;
+			this.btnOpen.Text = "Открыть изображение...";
+			this.btnOpen.UseVisualStyleBackColor = true;
+			this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(536, 344);
+			this.Controls.Add(this.grpFile);
+			this.Controls.Add(this.grpColor);
+			this.Controls.Add(this.pbImage);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.Text = "SMDtale ColGenerator";
+			((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+			this.grpColor.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pbColor)).EndInit();
+			this.grpFile.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pbImage;
+		private System.Windows.Forms.GroupBox grpColor;
+		private System.Windows.Forms.Button btnColorPicker;
+		private System.Windows.Forms.PictureBox pbColor;
+		private System.Windows.Forms.GroupBox grpFile;
+		private System.Windows.Forms.Button btnSave;
+		private System.Windows.Forms.Button btnOpen;
+	}
+}
+
---
+++ SMDtale ColGenerator/MainForm.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SMDtale_ColGenerator {
+	public partial class MainForm : Form {
+		public MainForm() {
+			InitializeComponent();
+		}
+
+		Bitmap mBitmap;
+		Color? mColor;
+		bool mIsPickerMode = false;
+
+		private void btnOpen_Click(object sender, EventArgs e) {
+			OpenFileDialog ofd = new OpenFileDialog();
+			ofd.Title = "Выберите графический файл";
+			if (ofd.ShowDialog() != DialogResult.OK) return;
+			try {
+				mBitmap = new Bitmap(ofd.FileName);
+				if (mBitmap.Width % 8 != 0) throw new Exception("Ширина изображения должна быть кратна 8!");
+				if (mBitmap.Height % 8 != 0) throw new Exception("Высота изображения должна быть кратна 8!");
+				pbImage.Image = mBitmap;
+				pbColor.Image = null;
+				if (pbImage.Width < 451)
+					Size = new Size(552, pbImage.Size.Height + 126);
+				else Size = new Size(pbImage.Size.Width + 40, pbImage.Size.Height + 126);
+				grpColor.Top = pbImage.Size.Height + 18;
+				grpFile.Top = pbImage.Size.Height + 18;
+				if (!grpColor.Enabled) {
+					grpColor.Enabled = true;
+					btnSave.Enabled = true;
+				}
+			} catch (Exception ex) {
+				mBitmap = null;
+				mColor = null;
+				pbImage.Image = mBitmap;
+				pbColor.Image = null;
+				pbImage.Size = new Size(512, 256);
+				Size = new Size(552, 382);
+				grpColor.Enabled = false;
+				btnSave.Enabled = false;
+				MessageBox.Show(ex.Message, "*_*", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+		private void btnSave_Click(object sender, EventArgs e) {
+			SaveFileDialog sfd = new SaveFileDialog();
+			sfd.Title = "Куда сохранить файл?";
+			sfd.Filter = "Бинарный файл|*.bin|Все файлы|*.*";
+			if (sfd.ShowDialog() != DialogResult.OK) return;
+			try {
+				FileStream fs = new FileStream(sfd.FileName, FileMode.Create);
+				byte[] width = BitConverter.GetBytes((ushort)mBitmap.Width / 8);
+				byte[] height = BitConverter.GetBytes((ushort)mBitmap.Height / 8);
+				if (BitConverter.IsLittleEndian) {
+					Array.Reverse(width);
+					Array.Reverse(height);
+					width = new byte[2] { width[2], width[3] };
+					height = new byte[2] { height[2], height[3] };
+				}
+				fs.Write(width, 0, 2);
+				fs.Write(height, 0, 2);
+				for (int y = 0; y < mBitmap.Height; y += 8) {
+					for (int x = 0; x < mBitmap.Width; x += 8) {
+						if (mBitmap.GetPixel(x, y) == mColor) fs.WriteByte(255);
+						else fs.WriteByte(0);
+					}
+				}
+				fs.Close();
+				MessageBox.Show("Операция успешно завершена!", "*_*", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			} catch (Exception ex) { MessageBox.Show(ex.Message, "*_*", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+		}
+
+		private void turnGUI(bool isEnabled) {
+			grpColor.Enabled = isEnabled;
+			grpFile.Enabled = isEnabled;
+		}
+		private void btnColorPicker_Click(object sender, EventArgs e) {
+			mIsPickerMode = true;
+			turnGUI(false);
+		}
+		private void pbImage_MouseMove(object sender, MouseEventArgs e) {
+			if (!mIsPickerMode) return;
+
+			mColor = mBitmap.GetPixel(e.X, e.Y);
+			pbColor.Image = GenerateColorTile((Color) mColor, 32);
+		}
+		private Bitmap GenerateColorTile(Color color, int size) {
+			Bitmap b = new Bitmap(size, size);
+			for (int y = 0; y < size; y++)
+				for (int x = 0; x < size; x++)
+					b.SetPixel(x, y, color);
+			return b;
+		}
+		private void pbImage_MouseClick(object sender, MouseEventArgs e) {
+			if (!mIsPickerMode) return;
+
+			mIsPickerMode = false;
+			turnGUI(true);
+		}
+
+	}
+}
